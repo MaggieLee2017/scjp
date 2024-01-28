19730a58a84411c53882d8319c03311deb6d54e1
Maggie Lee <madge29892@hotmail.com>
2024-01-28 19:22:01 +0800

    Maggie add.

--- src/chapter03exercises.java
+++ src/chapter03exercises.java
@@ -3,7 +3,7 @@
  * 修訂時間：2024/1
  */
 public class chapter03exercises {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Q01:「7+6-5*4/3%(2+1)的結果為何？」");
         System.out.print("Q01 結果：");
         Q01.showMe();
@@ -26,26 +26,35 @@ public class chapter03exercises {
         System.out.print("Q03 結果：");
         Q03.showMe();
 
+        System.out.println("////////////////////////////////////");
+        System.out.println("Q04:以下程式碼會出現什麼結果？");
+        System.out.println("\tint x=1, y=1;");
+        System.out.println("\tboolean b = ++x > ++y;");
+        System.out.println("\tSystem.out.println(b);");
+        System.out.print("Q04 結果：");
+        Q04.showMe();
 
-    }    
+    }
 
 }
 
 // Q01:「7+6-5*4/3%(2+1)的結果為何？」
-class Q01{
+class Q01 {
     chapter03exercises Q01 = new chapter03exercises();
-    public static void showMe(){
-        System.out.println(7+6-5*4/3%(2+1));
+
+    public static void showMe() {
+        System.out.println(7 + 6 - 5 * 4 / 3 % (2 + 1));
     }
 }
 
-//Q02. 以下程式碼會出現什麼結果？
+// Q02. 以下程式碼會出現什麼結果？
 // int i = 100;
 // String s = "10";
 // s += i;
 // System.out.println(s);
-class Q02{
+class Q02 {
     chapter03exercises q02 = new chapter03exercises();
+
     public static void showMe() {
         int i = 100;
         String s = "10";
@@ -54,19 +63,32 @@ class Q02{
     }
 }
 
-//Q03. 以下程式碼會出現什麼結果？
+// Q03. 以下程式碼會出現什麼結果？
 // boolean result;
 // int i = 1;
 // result = 1 == 2 && ++i >= 2;
 // System.out.println("result=" + result + ", i=" +i);
 class Q03 {
     chapter03exercises q03 = new chapter03exercises();
-    public static void showMe(){
-       boolean result;
-       int i = 1;
-       result = 1 == 2 && ++i >= 2;
-       System.out.println("result=" + result + ", i=" +i); 
-    }
 
-    
+    public static void showMe() {
+        boolean result;
+        int i = 1;
+        result = 1 == 2 && ++i >= 2;
+        System.out.println("result=" + result + ", i=" + i);
+    }
+}
+
+// Q04. 以下程式碼會出現什麼結果？
+// int x=1, y=1;
+// boolean b = ++x > ++y;
+// System.out.println(b);
+class Q04 {
+    chapter03exercises q04 = new chapter03exercises();
+
+    public static void showMe() {
+        int x = 1, y = 1;
+        boolean b = ++x > ++y;
+        System.out.println(b);
+    }
 }
