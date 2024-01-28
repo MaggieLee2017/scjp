d31dbfae5a3355223e12c66419d9c0200b277d65
Maggie Lee <madge29892@hotmail.com>
2024-01-28 19:49:44 +0800

    Maggie add.

--- src/chapter03exercises.java
+++ src/chapter03exercises.java
@@ -4,43 +4,43 @@
  */
 public class chapter03exercises {
     public static void main(String[] args) {
-        System.out.println("Q01:「7+6-5*4/3%(2+1)的結果為何？」");
-        System.out.print("Q01 結果：");
-        Q01.showMe();
+        System.out.println("Q3_1:「7+6-5*4/3%(2+1)的結果為何？」");
+        System.out.print("Q3_1 結果：");
+        Q3_1.showMe();
 
         System.out.println("////////////////////////////////////");
-        System.out.println("Q02:以下程式碼會出現什麼結果？");
+        System.out.println("Q3_2:以下程式碼會出現什麼結果？");
         System.out.println("\tint i = 100;");
         System.out.println("\tString s = \"10\";");
         System.out.println("\ts += i;");
         System.out.println("\tSystem.out.println(s);");
-        System.out.print("Q02 結果：");
-        Q02.showMe();
+        System.out.print("Q3_2 結果：");
+        Q3_2.showMe();
 
         System.out.println("////////////////////////////////////");
-        System.out.println("Q03:以下程式碼會出現什麼結果？");
+        System.out.println("Q3_3:以下程式碼會出現什麼結果？");
         System.out.println("\tboolean result;");
         System.out.println("\tint i = 1;");
         System.out.println("\tresult = 1 == 2 && ++i >= 2;");
         System.out.println("\tSystem.out.println(\"result=\" + result + \", i=\" +i);");
-        System.out.print("Q03 結果：");
-        Q03.showMe();
+        System.out.print("Q3_3 結果：");
+        Q3_3.showMe();
 
         System.out.println("////////////////////////////////////");
-        System.out.println("Q04:以下程式碼會出現什麼結果？");
+        System.out.println("Q3_4:以下程式碼會出現什麼結果？");
         System.out.println("\tint x=1, y=1;");
         System.out.println("\tboolean b = ++x > ++y;");
         System.out.println("\tSystem.out.println(b);");
-        System.out.print("Q04 結果：");
-        Q04.showMe();
+        System.out.print("Q3_4 結果：");
+        Q3_4.showMe();
 
     }
 
 }
 
 // Q01:「7+6-5*4/3%(2+1)的結果為何？」
-class Q01 {
-    chapter03exercises Q01 = new chapter03exercises();
+class Q3_1 {
+    chapter03exercises Q3_1 = new chapter03exercises();
 
     public static void showMe() {
         System.out.println(7 + 6 - 5 * 4 / 3 % (2 + 1));
@@ -52,8 +52,8 @@ class Q01 {
 // String s = "10";
 // s += i;
 // System.out.println(s);
-class Q02 {
-    chapter03exercises q02 = new chapter03exercises();
+class Q3_2 {
+    chapter03exercises q3_2 = new chapter03exercises();
 
     public static void showMe() {
         int i = 100;
@@ -68,8 +68,8 @@ class Q02 {
 // int i = 1;
 // result = 1 == 2 && ++i >= 2;
 // System.out.println("result=" + result + ", i=" +i);
-class Q03 {
-    chapter03exercises q03 = new chapter03exercises();
+class Q3_3 {
+    chapter03exercises q3_3 = new chapter03exercises();
 
     public static void showMe() {
         boolean result;
@@ -83,8 +83,8 @@ class Q03 {
 // int x=1, y=1;
 // boolean b = ++x > ++y;
 // System.out.println(b);
-class Q04 {
-    chapter03exercises q04 = new chapter03exercises();
+class Q3_4 {
+    chapter03exercises q3_4 = new chapter03exercises();
 
     public static void showMe() {
         int x = 1, y = 1;
---
+++ src/chapter04exercises.java
@@ -0,0 +1,29 @@
+public class chapter04exercises {
+    public static void main(String[] args) {
+        System.out.println("Q4_1:程式碼如下，請問最後結果為何？");
+        System.out.println("        int x=2;\n" + //
+                "        int y=1;\n" + //
+                "        if((x == 1) && (y == 2))\n" + //
+                "            System.out.print(\"x = 1, \");\n" + //
+                "        System.out.print(\"y = 2, \");\n" + //
+                "        if( x+y == 3)\n" + //
+                "            System.out.println(\"x+y = 3\");");
+        System.out.print("Q4_1 結果：");
+        Q4_1.showMe();
+
+        System.out.println("////////////////////////////////////");
+    }
+}
+
+class Q4_1{
+    chapter04exercises q4_1 = new chapter04exercises();
+    public static void showMe() {
+        int x=2;
+        int y=1;
+        if((x == 1) && (y == 2))
+            System.out.print("x = 1, ");
+        System.out.print("y = 2, ");
+        if( x+y == 3)
+            System.out.println("x+y = 3");
+    }
+}
